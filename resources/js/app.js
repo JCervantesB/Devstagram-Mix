c00a9b8ffd9829c99b35098020df028a44a8e068
Julio Cervantes <imjcervantes@gmail.com>
2022-12-08 17:15:53 -0700

    debug

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,47 +2,50 @@ import Dropzone from "dropzone";
 
 Dropzone.autoDiscover = false;
 
-if(document.querySelector('#dropzone')) {
+const dropzone = new Dropzone("#dropzone", {
+    dictDefaultMessage: "Sube aquí tu imagen",
+    acceptedFiles: ".png,.jpg,.jpeg,.gif",
+    addRemoveLinks: true,
+    dictRemoveFile: "Borrar Archivo",
+    maxFiles: 1,
+    uploadMultiple: false,
 
-    const dropzone = new Dropzone("#dropzone", {
-        dictDefaultMessage: "Sube aquí tu imagen",
-        acceptedFiles: ".png,.jpg,.jpeg,.gif",
-        addRemoveLinks: true,
-        dictRemoveFile: "Borrar Archivo",
-        maxFiles: 1,
-        uploadMultiple: false,
+    init: function () {
+        if (document.querySelector('[name="imagen"]').value.trim()) {
+            const imagenPublicada = {};
+            imagenPublicada.size = 1234;
+            imagenPublicada.name =
+                document.querySelector('[name="imagen"]').value;
 
-        init: function () {
-            if (document.querySelector('[name="imagen"]').value.trim()) {
-                const imagenPublicada = {};
-                imagenPublicada.size = 1234;
-                imagenPublicada.name =
-                    document.querySelector('[name="imagen"]').value;
+            this.options.addedfile.call(this, imagenPublicada);
 
-                this.options.addedfile.call(this, imagenPublicada);
+            this.options.thumbnail.call(
+                this,
+                imagenPublicada,
+                `/uploads/${imagenPublicada.name}`
+            );
 
-                this.options.thumbnail.call(
-                    this,
-                    imagenPublicada,
-                    `/uploads/${imagenPublicada.name}`
-                );
+            imagenPublicada.previewElement.classList.add(
+                "dz-success",
+                "dz-complete"
+            );
+        }
+    },
+});
 
-                imagenPublicada.previewElement.classList.add(
-                    "dz-success",
-                    "dz-complete"
-                );
-            }
-        },
-    });
+dropzone.on("success", function (file, response) {
+    document.querySelector('[name="imagen"]').value = response.imagen;
+});
 
-    dropzone.on("success", function (file, response) {
-        document.querySelector('[name="imagen"]').value = response.imagen;
-    });
+dropzone.on("removedfile", function () {
+    document.querySelector('[name="imagen"]').value = "";
+});
 
-    dropzone.on("removedfile", function () {
-        document.querySelector('[name="imagen"]').value = "";
-    });
-}
+dropzone.on('error', function(file, mensaje) {
+    console.log(file)
+    console.log(mensaje)
+});
+
 
 if(document.querySelector('.bg-green-500')) {
     setTimeout(() => {
